5174b25864114ffd4f12cc3e227c793415637998
MaksimDemin <mdeminalex@icloud.com>
2019-10-07 18:19:14 +0800

    changed HW5.5

--- src/Main.java
+++ src/Main.java
@@ -63,11 +63,21 @@ public class Main {
 
 
     private static void searchBinary(String enterNumber) { // метод поиска номера бинарным способом
-        long startTime = System.nanoTime();
-        {
+            long startTime = System.nanoTime();
+            if (binarySearch(coolNumbers, enterNumber)) {
             long finishTime = System.nanoTime() - startTime;
-                System.out.println("Number " + enterNumber + " found by Binary search, time spent   - " + finishTime + " ns.");
+            System.out.println("Number " + enterNumber + " found by Binary search, time spent   - " + finishTime + " ns.");
         }
+            else {
+                long finishTime = System.nanoTime() - startTime;
+                System.out.println("Number " + enterNumber + " don't found by Binary search, time spent   - " + finishTime + " ns.");
+            }
+    }
+
+
+    private static boolean binarySearch(ArrayList<String> arrayList, String enterNumber) { // метод для проверки наличия искомого номера в пределах списка
+        int a = Collections.binarySearch(arrayList, enterNumber);
+        return a >= 0;
     }
 
 
@@ -93,32 +103,5 @@ public class Main {
                 System.out.println("Number " + enterNumber + " don't found by TreeSet search, time spent - " + finishTime + " ns.");
             }
     }
-
-
-
-//    public static void binarySearch(ArrayList<String> input, String number) {
-//        String  first = input.get(0);
-//        String  last = input.get(coolNumbers.size() - 1);
-//        String  middle = input.get(coolNumbers.size() / 2);
-//        while (input.indexOf("1") <= input.lastIndexOf("5")) {
-//            if (Collections.binarySearch(coolNumbers, middle) < Collections.binarySearch(coolNumbers, number)) {
-//                first = input.get((coolNumbers.size() / 2) + 1);
-//            } else if (middle.equals(number)) {
-//                System.out.printf(number + " found at location %s %n", middle);
-//                break;
-//            } else {
-//                last = input.get((coolNumbers.size() / 2) - 1);
-//            }
-//            middle = input.get(coolNumbers.size() / 2);
-//        }
-//        if (Collections.binarySearch(coolNumbers, first) > Collections.binarySearch(coolNumbers, last)) {
-//            System.out.println(number + " is not present in the list.\n");
-//        }
-//    }
-
-
-
-
-
 
 }
